8eed95054cdaaf281f2dda3f20fcb1835a2570e2
Barney <B.Wilton1993@gmail.com>
2023-04-21 12:42:30 +0100

    wrote tests for placing ships on the board

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -8,14 +8,58 @@ RSpec.describe Game do
     end
   end
 
-  it "can place a single ship" do
-    game_object = Game.new
-    game_object.place_ship(
-      length: 2,
-      orientation: {"v" => :vertical, "h" => :horizontal}.fetch("h"),
-      row: 2.to_i,
-      col: 2.to_i
-    )
-    expect(game_object.placed_ships).to eq [[2,2],[2,3],[2,4]]
+  context "can place ships" do
+    it "can place a single ship" do
+      game_object = Game.new
+      ship_1 = {
+        length: 2,
+        orientation: :horizontal,
+        row: 2,
+        col: 2
+      }
+      game_object.place_ship(ship_1)
+      expect(game_object.placed_ships).to eq [[2,2],[2,3],[2,4]]
+    end
+
+    
+    it "can place multiple ships" do
+      game_object = Game.new
+      ship_1 = {
+        length: 2,
+        orientation: :horizontal,
+        row: 2,
+        col: 2
+      }
+      ship_2 = {
+        length: 2,
+        orientation: :horizontal,
+        row: 3,
+        col: 2
+      }
+      game_object.place_ship(ship_1)
+      game_object.place_ship(ship_2)
+      expect(game_object.placed_ships).to eq [[2,2], [2,3], [2,4], [3,2], [3,3], [3,4]]
+    end
+  end
+  
+  context "cant place ships" do
+    it "won't place a ship if space already occupied ship" do
+      game_object = Game.new
+      ship_1 = {
+        length: 2,
+        orientation: :horizontal,
+        row: 2,
+        col: 2
+      }
+      ship_2 = {
+        length: 2,
+        orientation: :vertical,
+        row: 2,
+        col: 2
+      }
+      game_object.place_ship(ship_1)
+      game_object.place_ship(ship_2)
+      expect(game_object.placed_ships).to eq [[2,2],[2,3],[2,4]]
+    end
   end
 end
