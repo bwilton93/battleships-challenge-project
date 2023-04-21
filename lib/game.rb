59939fd664b5271e20bb322c857f7ccf61a3ad46
Barney <B.Wilton1993@gmail.com>
2023-04-21 10:42:14 +0100

    place_ship method in game.rb can now accept parameters

--- lib/game.rb
+++ lib/game.rb
@@ -1,14 +1,16 @@
 class Game
   def unplaced_ships
-    []
+    ships = ["SS", "SSSSS"]
   end
 
   def rows
+    return 10
   end
 
   def cols
+    return 10
   end
 
-  def place_ship
+  def place_ship(parameters = {})
   end
 end
