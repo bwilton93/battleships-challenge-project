cfa79f1d4ceeacda6245f3d46159329e49508fcd
Barney <B.Wilton1993@gmail.com>
2023-04-21 12:35:32 +0100

    wrote test for being able to place a single ship

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,4 +7,15 @@ RSpec.describe Game do
       expect(game_object.placed_ships).to eq []
     end
   end
+
+  it "can place a single ship" do
+    game_object = Game.new
+    game_object.place_ship(
+      length: 2,
+      orientation: {"v" => :vertical, "h" => :horizontal}.fetch("h"),
+      row: 2.to_i,
+      col: 2.to_i
+    )
+    expect(game_object.placed_ships).to eq [[2,2],[2,3],[2,4]]
+  end
 end
