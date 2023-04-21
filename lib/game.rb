118d5b231ae129338d22f87d83326d4af922506f
Barney <B.Wilton1993@gmail.com>
2023-04-21 10:59:11 +0100

    can extract ship placemet information from player inputs

--- lib/game.rb
+++ lib/game.rb
@@ -12,5 +12,31 @@ class Game
   end
 
   def place_ship(parameters = {})
+    @ship_start_position = [parameters[:row], parameters[:col]]
+    
+    case parameters[:orientation]
+    when :horizontal
+      @ship_end_position = [parameters[:row], parameters[:col] + parameters[:length]]
+    when :vertical
+      @ship_end_position = [parameters[:row] - parameters[:length], parameters[:col]]
+    end
+  end
+
+  def ship_at?(x, y)
+
   end
 end
+
+# game_object = Game.new
+# game_object.place_ship(
+#   length: 2,
+#   orientation: {"v" => :vertical, "h" => :horizontal}.fetch("h"),
+#   row: 2.to_i,
+#   col: 2.to_i
+# )
+# game_object.place_ship(
+#   length: 2,
+#   orientation: {"v" => :vertical, "h" => :horizontal}.fetch("v"),
+#   row: 2.to_i,
+#   col: 2.to_i
+# )
