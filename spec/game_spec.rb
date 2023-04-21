d45492eedeeedb07135de898d4618e98209fc16b
Barney <B.Wilton1993@gmail.com>
2023-04-21 12:12:23 +0100

    Game object initializes with no placed ships

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,6 +3,8 @@ require 'game'
 RSpec.describe Game do
   context "initializes" do
     it "with an empty board of placed ships" do
+      game_object = Game.new
+      expect(game_object.placed_ships).to eq []
     end
   end
 end
